5ec90db0424db34ad68b090642ab7a1dc423aca1
Isaiah Kahler <isaiahkahler@thicc-mac.local>
2018-12-18 09:38:33 -0500

    printing example > instructions

---
+++ Instructions.py
@@ -0,0 +1,87 @@
+"""Mr. Myerscough's printing example:
+This program will teach you how to output text onto the screen."""
+
+
+# first example
+
+print("Hello World!")
+
+# more examples
+
+print("Mr. Myerscough")
+
+print      ("hello world")
+
+# ERRORS : 
+
+         print("hello world")
+
+Print("hello world")
+
+print(hello world)
+
+# other examples
+
+print(500)
+
+print(30 * 27 + 14)
+
+print(True)
+
+print(None)
+
+
+"""
+video points: *NEEDS REVISION*
+
+*make an introduction, or something, probably*
+
+how to write instructions / commands:
+
+-first type the instruction name, in this case "print"
+
+-a set of parenthesis after the name of the instruction is what tells the
+program to run that instruction
+
+-inside the parenthesis put a set of double quotation marks and type something inside 
+(we'll type hello world)
+
+-the value or values inside the set of parenthesis is the data you give to that command
+
+-the end of the instruction is the end of the line. when you create a new line, you can
+write more commands
+
+-run your program by right clicking and selecting "run code"
+
+-the output is shown in this part, called the terminal
+
+-as you can see, the program has printed exactly what we commanded it to. cool.
+
+-let's go back to the program again. since we know that a program is just a **set** of instructions,
+we can type as many "prints" as we want. (go back and print your name, then run the program again)
+a second print will do what you'd probably expect
+
+-if your program didn't run, and you have an error, there are a lot of reasons why that might be
+
+-programming languages can be very picky for how they instructions are written. for example,
+you can put as many spaces between the command name and the set of parenthesis (demo),
+but if you accidentally put a space or tab before the command name, it'll error. 
+
+-it also cares about the way you write the command name. it's case-sensitive, so if i type
+"print" with a capital "P", the program will error, because it couldn't find a command with that name
+
+-and if i get rid of the quotation marks, the program gets confused again
+
+-different commands take different types of data. this is called a string, which is made up 
+of letters and characters
+
+-"print" will also take numbers (demo), expressions, and other values.
+
+-awesome. so know we know how to write commands and give them different kinds of data.
+
+-that's all for this lesson, make sure to play around with your own programs and learn a little on your own
+
+*outro or something, lol*
+
+
+"""
